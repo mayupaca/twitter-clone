21420fba9c666a2cac40b069467f9d6d5b7147e0
Mayu Suzumura <mayumurasuzu@gmail.com>
2022-05-22 16:31:57 -0400

    Installed material UI

---
+++ src/components/sidebar/Sidebar.js
@@ -0,0 +1,32 @@
+import React from "react";
+import TwitterIcon from '@mui/icons-material/Twitter';
+import { SidebarOption } from "./SidebarOption";
+import HomeIcon from '@mui/icons-material/Home';
+import SearchIcon from '@mui/icons-material/Search';
+import NotificationsNoneIcon from "@mui/icons-material/NotificationsNone";
+import MailOutlineIcon from '@mui/icons-material/MailOutline';
+import BookmarkBorderIcon from '@mui/icons-material/BookmarkBorder';
+import ListAltIcon from "@mui/icons-material/ListAlt";
+import PermIdentityIcon from '@mui/icons-material/PermIdentity';
+import MoreHorizIcon from '@mui/icons-material/MoreHoriz';
+
+export const Sidebar = () => {
+  return (
+  <div className="sidebar">
+    {/* twitter icon */}
+    <TwitterIcon className="sidebar_twitterIcon" />
+    {/* SidebarOption */}
+    <SidebarOption text="Home" Icon={HomeIcon} />
+    <SidebarOption text="Explore" Icon={SearchIcon} />
+    <SidebarOption text="Notifications" Icon={NotificationsNoneIcon} />
+    <SidebarOption text="Messages" Icon={MailOutlineIcon} />
+    <SidebarOption text="Bookmarks" Icon={BookmarkBorderIcon} />
+    <SidebarOption text="Lists" Icon={ListAltIcon} />
+    <SidebarOption text="Profile" Icon={PermIdentityIcon} />
+    <SidebarOption text="More" Icon={MoreHorizIcon} />
+    
+
+    {/* twitte button */}
+  </div>
+  )
+};
